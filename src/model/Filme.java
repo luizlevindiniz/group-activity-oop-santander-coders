07b2b24b37a60fcf0b01833f9cba9ffc70906fee
luizlevindiniz <luizd97@gmail.com>
2024-02-05 22:03:07 -0300

    add models attributes

--- src/model/Filme.java
+++ src/model/Filme.java
@@ -1,4 +1,71 @@
 package model;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+
 public class Filme {
+    private int id;
+    private String titulo;
+    private LocalDate dataDeLancamento;
+    private String descricao;
+    private BigDecimal orcamento;
+    private List<Ator> atores;
+    private List<Diretor> diretores;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public LocalDate getDataDeLancamento() {
+        return dataDeLancamento;
+    }
+
+    public void setDataDeLancamento(LocalDate dataDeLancamento) {
+        this.dataDeLancamento = dataDeLancamento;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public BigDecimal getOrcamento() {
+        return orcamento;
+    }
+
+    public void setOrcamento(BigDecimal orcamento) {
+        this.orcamento = orcamento;
+    }
+
+    public List<Ator> getAtores() {
+        return atores;
+    }
+
+    public void setAtores(List<Ator> atores) {
+        this.atores = atores;
+    }
+
+    public List<Diretor> getDiretores() {
+        return diretores;
+    }
+
+    public void setDiretores(List<Diretor> diretores) {
+        this.diretores = diretores;
+    }
 }
