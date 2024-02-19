fbdaec267059a7912a143ae158ee5bf57622686b
ryangois <ryangoisdev@gmail.com>
2024-02-19 20:41:09 -0300

    exibirDetalhes implementation

--- src/controllers/FilmesController.java
+++ src/controllers/FilmesController.java
@@ -2,23 +2,26 @@ package controllers;
 
 import model.Filme;
 import repository.FilmesRepository;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.Scanner;
 import java.util.stream.Collectors;
 
 public class FilmesController implements FilmesRepository {
     private static final List<Filme> listaDeFilmes = new ArrayList<>();
     private static FilmesController instance;
 
-    private FilmesController(){
+    private FilmesController() {
 
     }
-    public static FilmesController getInstance(){
-        if (instance == null){
+
+    public static FilmesController getInstance() {
+        if (instance == null) {
             instance = new FilmesController();
         }
-        return  instance;
+        return instance;
     }
 
     @Override
@@ -31,7 +34,7 @@ public class FilmesController implements FilmesRepository {
     }
 
     @Override
-    public Filme deletar(int id){
+    public Filme deletar(int id) {
         Optional<Filme> filmeOptional = listaDeFilmes.stream().filter(f -> f.getId() == id).findFirst();
 
         if (filmeOptional.isPresent()) {
@@ -44,8 +47,8 @@ public class FilmesController implements FilmesRepository {
         }
     }
 
-    public Filme alteraNome(int id, String nome){
-        Optional<Filme> filmeOptional  = listaDeFilmes.stream().filter(f -> f.getId() == id).findFirst();
+    public Filme alteraNome(int id, String nome) {
+        Optional<Filme> filmeOptional = listaDeFilmes.stream().filter(f -> f.getId() == id).findFirst();
         if (filmeOptional.isPresent()) {
             Filme filme = filmeOptional.get();
             filme.setTitulo(nome);
@@ -55,13 +58,38 @@ public class FilmesController implements FilmesRepository {
         }
     }
 
-    public List<Filme> listarTodos(){
+    public List<Filme> listarTodos() {
         return listaDeFilmes;
     }
 
-    public List<Filme> pesquisarPorNome(String nome){
+    public List<Filme> pesquisarPorNome(String nome) {
         return listaDeFilmes.stream().filter(filme ->
                 filme.getTitulo().contains(nome)).collect(Collectors.toList());
     }
 
+    public void exibirDetalhes() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Por favor, insira o ID do filme para ver os detalhes:");
+        int idFilme = scanner.nextInt();
+
+        Optional<Filme> filmeOptional = listaDeFilmes.stream().filter(f -> f.getId() == idFilme).findFirst();
+
+        if (filmeOptional.isPresent()) {
+            Filme filme = filmeOptional.get();
+            System.out.println("Detalhes do Filme:");
+            System.out.println("Título: " + filme.getTitulo());
+            System.out.println("Data de Lançamento: " + filme.getDataDeLancamento());
+            System.out.println("Descrição: " + filme.getDescricao());
+            System.out.println("Orçamento: " + filme.getOrcamento());
+
+            System.out.println("Atores:");
+            filme.getAtores().forEach(ator -> System.out.println("- " + ator.getNome()));
+
+            System.out.println("Diretores:");
+            filme.getDiretores().forEach(diretor -> System.out.println("- " + diretor.getNome()));
+        } else {
+            System.out.println("Filme com ID " + idFilme + " não encontrado");
+        }
+    }
 }
+
